100f4c134b88c7f682701e04bac23d4beb5cda74
IronFeniks <iron.shizo@gmail.com>
2026-03-26 10:33:27 +0400

    Update and rename handlers/calculator.py to handlers/calculator/__init__.py

---
+++ handlers/calculator/__init__.py
@@ -0,0 +1,15 @@
+from .handlers import (
+    start_calculator,
+    calculator_text_handler,
+    calculator_callback_handler,
+    cancel_calculator,
+    help_calculator
+)
+
+__all__ = [
+    'start_calculator',
+    'calculator_text_handler',
+    'calculator_callback_handler',
+    'cancel_calculator',
+    'help_calculator'
+]
