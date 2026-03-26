2a6cdac657e00bcda8b32a315af2ef50bb3b44b7
IronFeniks <iron.shizo@gmail.com>
2026-03-26 14:42:11 +0400

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,23 @@
 from .router import router_handler
-from .calculator import start_calculator, calculator_text_handler, calculator_callback_handler, cancel_calculator, help_calculator
+from .calculator import (
+    start_calculator,
+    calculator_text_handler,
+    calculator_callback_handler,
+    cancel_calculator,
+    help_calculator
+)
 from .admin import start_admin, admin_text_handler, admin_callback_handler, cancel_admin, help_admin
+
+__all__ = [
+    'router_handler',
+    'start_calculator',
+    'calculator_text_handler',
+    'calculator_callback_handler',
+    'cancel_calculator',
+    'help_calculator',
+    'start_admin',
+    'admin_text_handler',
+    'admin_callback_handler',
+    'cancel_admin',
+    'help_admin'
+]
